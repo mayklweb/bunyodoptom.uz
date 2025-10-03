035e668b8282dacc69a3943a31cd5bf360c2dec6
mayklweb <muhammadevvv@gmail.com>
2025-10-03 22:30:22 +0500

    next

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,47 @@ const montserrat = Montserrat({
   subsets: ["latin"],
 });
 
-
 export const metadata: Metadata = {
-  title: "Bunyod Optom | Дистрибьюторская фирма",
-  description: "Bunyod Optom - Eng yaxshi narxlar, keng assortiment, tez yetkazib berish",
+  title: "Bunyod Optom distribyutorlik firmasi",
+  description:
+    "Bunyod Optom - Eng yaxshi narxlar, keng assortiment, tez yetkazib berish",
   icons: {
     icon: "/logo1.svg",
+  },
+  viewport: {
+    width: "device-width",
+    initialScale: 1,
+    maximumScale: 1,
+  },
+  metadataBase: new URL("https://bunyodoptom.uz"),
+  openGraph: {
+    title: "Bunyod Optom distribyutorlik firmasi",
+    description:
+      "Bunyod Optom - Eng yaxshi narxlar, keng assortiment, tez yetkazib berish",
+    url: "https://bunyodoptom.uz",
+    siteName: "Bunyod Optom",
+    images: [
+      {
+        url: "/logo1.svg",
+        width: 800,
+        height: 600,
+      },
+      {
+        url: "/logo1.svg",
+        width: 1800,
+        height: 1600,
+        alt: "My custom alt",
+      },
+    ],
+    locale: "uz_UZ",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Bunyod Optom distribyutorlik firmasi",
+    description:
+      "Bunyod Optom - Eng yaxshi narxlar, keng assortiment, tez yetkazib berish",
+    images: ["/logo1.svg"],
   },
 };
 export default function RootLayout({
@@ -24,15 +59,11 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${montserrat.variable}  antialiased`}
-      >
+      <body className={`${montserrat.variable}  antialiased`}>
         <Providers>
           <div className="min-h-[100dvh] flex flex-col">
             <Header />
-            <main className="flex flex-auto flex-col">
-              {children}
-            </main>
+            <main className="flex flex-auto flex-col">{children}</main>
             <Footer />
           </div>
         </Providers>
