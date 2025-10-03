cb2010b0db87f8999d28cd37b725886cdafa9f6a
mayklweb <muhammadevvv@gmail.com>
2025-10-03 16:49:10 +0500

    next

--- src/app/categories/page.tsx
+++ src/app/categories/page.tsx
@@ -36,7 +36,7 @@ function Categories() {
           </div>
           <div className="mt-6 mb-10 grid grid-cols-2 lg:grid-cols-4 gap-4">
             {products.map((product, key) => (
-              <Link key={key} href={`/product/${product.id}`}>
+              <Link key={key} href={`/product/${product.name.toLowerCase()}`}>
                 <div className="flex flex-col gap-2 rounded-xl cursor-pointer">
                   <div className="w-full h-full rounded-xl overflow-hidden">
                     <Image
