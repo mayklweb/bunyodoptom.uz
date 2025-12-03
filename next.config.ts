ee7214d5585f1b34a2ae810729f749c810d74406
Muhammad <muhammadevvv@gmail.com>
2025-12-03 12:12:37 +0500

    m

--- next.config.ts
+++ next.config.ts
@@ -12,48 +12,29 @@ const nextConfig = {
             value: `
               default-src 'self';
               
-              /* JS scripts */
-              script-src 
-                'self' 
-                'unsafe-inline' 
-                https://www.googletagmanager.com 
-                https://www.google-analytics.com 
-                https://maps.googleapis.com;
-
-              /* Styles */
-              style-src 
-                'self' 
-                'unsafe-inline';
-
-              /* Images */
-              img-src 
-                'self' 
-                blob: 
-                data: 
-                https:;
-
-              /* Fonts */
-              font-src 
-                'self' 
-                data: 
-                https:;
-
-              /* Connections (Very Important!) */
-              connect-src 
-                'self' 
-                https://api.bunyodoptom.uz 
-                http://localhost:4000 
-                https://www.google-analytics.com 
-                https://www.googletagmanager.com;
-
-              /* iFrames */
-              frame-src 
-                'self' 
-                https://www.googletagmanager.com;
-
+              script-src 'self' 'unsafe-inline' 'unsafe-eval'
+                https://www.googletagmanager.com
+                https://www.google-analytics.com
+                https://my.click.uz
+                blob:;
+              
+              style-src 'self' 'unsafe-inline';
+              
+              img-src 'self' blob: data: https:;
+              
+              font-src 'self' data: https:;
+              
+              connect-src 'self'
+                https://api.bunyodoptom.uz
+                http://localhost:4000
+                https://www.google-analytics.com
+                https://www.googletagmanager.com
+                https://my.click.uz;
+              
+              frame-src 'self' https://www.googletagmanager.com;
+              
               frame-ancestors 'self';
-
-              /* Workers */
+              
               worker-src 'self' blob:;
             `.replace(/\s+/g, " "),
           },
